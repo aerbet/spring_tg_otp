cdf7f02c19f57d453fe20d897aee11d8da8abc40
Erbol Zhaparov <ae.270718@gmail.com>
2026-02-23 22:33:25 +0600

    initial commit

---
+++ src/main/java/kg/zhaparov/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package kg.zhaparov.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/zhaparov/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.zhaparov.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
